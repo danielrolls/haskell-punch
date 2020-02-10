c05b4fa26efca76229726ee9dbb1de05e92e1b62
Daniel Rolls <daniel.rolls.27@googlemail.com>
2020-02-10 09:21:57 +0000

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Network.Wreq
+import Control.Lens
+import Control.Lens.Prism
+import Data.Aeson
+import Data.Aeson.Lens
+import System.Process (readProcessWithExitCode)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Control.Foldl as Fold
+
+main :: IO ()
+main = print "Nothing to see"
+
+pbcopy = readProcessWithExitCode "/usr/bin/pbcopy" []
+pbpaste = (\(_,b,_) -> b) <$> readProcessWithExitCode "/usr/bin/pbpaste" [] ""
