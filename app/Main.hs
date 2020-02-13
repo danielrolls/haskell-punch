692e4c181d91fc10f28aca4edd84873c501e057c
Daniel Rolls <daniel.rolls.27@googlemail.com>
2020-02-13 16:38:54 +0000

    import Data.Sort

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Control.Lens
 import Control.Lens.Prism
 import Data.Aeson
 import Data.Aeson.Lens
+import Data.Sort
 import System.Process (readProcessWithExitCode)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
